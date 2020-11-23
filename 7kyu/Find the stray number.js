1f300bd3b68ef6239e8c5c6d64e791f114a0acb3
CJ-Yang <cj.yang0225@gmail.com>
2020-11-23 16:15:26 +0800

    Create Find the stray number.js

---
+++ 7kyu/Find the stray number.js
@@ -0,0 +1,31 @@
+/* You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+
+Examples
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3 
+------------------------------------------------------------------------------*/
+
+function stray(numbers) {
+  let strayNumberCount = {};
+
+  for (let i = 0; i < numbers.length; i++)
+    // 有值：+1，無值：設為1
+    strayNumberCount[numbers[i]] = strayNumberCount[numbers[i]] + 1 || 1;
+
+  // '+'代表 String -> Number
+  return +Object.keys(strayNumberCount).find(
+    (key) => strayNumberCount[key] == 1
+  );
+}
+
+// function stray(numbers) {
+//   numbers = numbers.sort();
+//   return numbers[0] === numbers[1] ? numbers[numbers.length - 1] : numbers[0];
+// }
+
+/* 當 odd-length array of integers */
+// const stray = (numbers) => numbers.reduce((pre, val) => pre ^ val);
