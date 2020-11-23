d80c12cfdf1fd15bfb598ddf51a36f38c194682e
CJ-Yang <cj.yang0225@gmail.com>
2020-11-23 16:20:20 +0800

    Update Anagram Detection.js

--- 7kyu/Anagram Detection.js
+++ 7kyu/Anagram Detection.js
@@ -1,4 +1,5 @@
-/* An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+/* 
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
 
 Note: anagrams are case insensitive
 
