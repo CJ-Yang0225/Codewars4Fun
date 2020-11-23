dce637d18a4c620478efc32ad367886456e2c6ac
CJ-Yang <cj.yang0225@gmail.com>
2020-11-23 16:19:57 +0800

    Update Find the stray number.js

--- 7kyu/Find the stray number.js
+++ 7kyu/Find the stray number.js
@@ -1,4 +1,5 @@
-/* You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+/* 
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
 
 Complete the method which accepts such an array, and returns that single different number.
 
