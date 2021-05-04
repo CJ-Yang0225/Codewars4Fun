13c3ff07ec6ec0291105d808271471a92ce7514d
CJ-Yang0225 <cj.yang0225@gmail.com>
2021-05-04 22:12:12 +0800

    Create Data Reverse.js

---
+++ 6kyu/Data Reverse.js
@@ -0,0 +1,30 @@
+/* 透過 Two Pointers 方式降低時間複雜度 */
+function dataReverse(data) {
+  let left = 0;
+  let right = data.length - 8;
+  const segmentData = new Array(data.length).fill(null);
+
+  if (!data.length) return [];
+
+  while (left <= right) {
+    for (let i = 0; i < 8; i++) {
+      segmentData[right + i] = data[left + i];
+      segmentData[left + i] = data[right + i];
+    }
+    left += 8;
+    right -= 8;
+  }
+
+  return segmentData;
+}
+
+/* 直覺的寫法 */
+// function dataReverse(data) {
+//   let results = [];
+//   const length = data.length;
+//   for (let i = 1; i <= length / 8; i++) {
+//     let pointer = length - i * 8
+//     results.push(...data.slice(pointer, pointer + 8));
+//   }
+//   return results;
+// }
